2b38000f2363ee1b1f9bb87cc66999301e562083
Sam Wilkinson <admin@samwilki.co.uk>
2012-03-28 22:52:25 +0100

    Changed username and password values

--- settings.py
+++ settings.py
@@ -19,8 +19,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'gluanet_svn',                      # Or path to database file if using sqlite3.
-        'USER': creds.uname,                      # Not used with sqlite3.
-        'PASSWORD': creds.pw,                  # Not used with sqlite3.
+        'USER': creds.username,                      # Not used with sqlite3.
+        'PASSWORD': creds.password,                  # Not used with sqlite3.
         'HOST': creds.host,                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
